d837366aa20df782024c0dcc38ec082535744ee3
Ifechukwu Ebenmelu <ifechukwu.ebenmelu@rivy.co>
2026-08-20 00:42:14 +0100

    Add temporary work in progress site

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
 // date
 const dateBegin = document.querySelector(".date__begin");
 const dateEnd = document.querySelector(".date__end");
-
 const thisYear = new Date(Date.now()).getFullYear();
 
 if (thisYear != dateBegin.innerText) {
   dateEnd.innerText = thisYear;
-}
 
-const handleFirstTab = (e) => {
-  if (e.key === "Tab") {
-    document.body.classList.add("user-is-tabbing");
+  const handleFirstTab = (e) => {
+    if (e.key === "Tab") {
+      document.body.classList.add("user-is-tabbing");
 
-    window.removeEventListener("keydown", handleFirstTab);
-    window.addEventListener("mousedown", handleMouseDownOnce);
-  }
-};
+      window.removeEventListener("keydown", handleFirstTab);
+      window.addEventListener("mousedown", handleMouseDownOnce);
+    }
+  };
 
-const handleMouseDownOnce = () => {
-  document.body.classList.remove("user-is-tabbing");
+  const handleMouseDownOnce = () => {
+    document.body.classList.remove("user-is-tabbing");
 
-  window.removeEventListener("mousedown", handleMouseDownOnce);
+    window.removeEventListener("mousedown", handleMouseDownOnce);
+    window.addEventListener("keydown", handleFirstTab);
+  };
+
   window.addEventListener("keydown", handleFirstTab);
-};
 
-window.addEventListener("keydown", handleFirstTab);
+  const backToTopButton = document.querySelector(".back-to-top");
+  let isBackToTopRendered = false;
 
-const backToTopButton = document.querySelector(".back-to-top");
-let isBackToTopRendered = false;
+  let alterStyles = (isBackToTopRendered) => {
+    backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
+    backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
+    backToTopButton.style.transform = isBackToTopRendered ? "scale(1)" : "scale(0)";
+  };
 
-let alterStyles = (isBackToTopRendered) => {
-  backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
-  backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
-  backToTopButton.style.transform = isBackToTopRendered ? "scale(1)" : "scale(0)";
-};
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 700) {
-    isBackToTopRendered = true;
-    alterStyles(isBackToTopRendered);
-  } else {
-    isBackToTopRendered = false;
-    alterStyles(isBackToTopRendered);
-  }
-});
-
-const skillSection = document.querySelector(".skills");
-const skillBars = document.querySelectorAll(".skill");
-
-new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      skillBars.forEach((skillBar) => skillBar.classList.add("animate-to-right"));
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 700) {
+      isBackToTopRendered = true;
+      alterStyles(isBackToTopRendered);
+    } else {
+      isBackToTopRendered = false;
+      alterStyles(isBackToTopRendered);
     }
   });
-}).observe(skillSection);
+
+  const skillSection = document.querySelector(".skills");
+  const skillBars = document.querySelectorAll(".skill");
+
+  new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        skillBars.forEach((skillBar) => skillBar.classList.add("animate-to-right"));
+      }
+    });
+  }).observe(skillSection);
+}
+
